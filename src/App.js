33a3efb1664cdf28c75be7afcfcc79c71891a1a1
lilu580 <bodia-00@ukr.net>
2024-08-27 06:17:27 +0300

    added styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import './App.css'
 
 function App() {
   const [guess, setGuess] = useState('');
@@ -21,22 +24,54 @@ function App() {
     setGuess('');
   };
 
+  const handleChange = (value) => {
+    if (value > 100) {
+      return setGuess(100)
+    }
+
+    if (value < 0) {
+      return setGuess(0)
+    }
+
+    setGuess(value)
+  }
+
   useEffect(() => {
     startNewGame();
   }, []); 
 
   return (
-    <div>
+    <div className='container'>
       <h1>Вгадай число</h1>
-      <input
-        type="number"
-        value={guess}
-        onChange={(e) => setGuess(e.target.value)}
+      <TextField 
+        type='number' 
+        value={guess} 
+        onChange={(e) => handleChange(e.target.value)}
         disabled={gameOver}
       />
-      <button onClick={handleGuess} disabled={gameOver}>Відправити спробу</button>
+      <Button 
+        onClick={handleGuess} 
+        disabled={gameOver} 
+        variant="contained" 
+        className='button'
+        sx={{
+          width: 210,
+          marginTop: 2,
+          '& .MuiOutlinedInput-root': { // стилі для кореневого елемента input
+            '& fieldset': {
+              borderColor: 'red', // червоний колір бордюра
+            },
+            '&:hover fieldset': {
+              borderColor: 'blue', // синій колір бордюра при наведенні
+            },
+            '&.Mui-focused fieldset': {
+              borderColor: 'green', // зелений колір бордюра при фокусі
+            },
+          },
+        }}
+      >Відправити спробу</Button>
       <div>{result}</div>
-      {gameOver && <button onClick={startNewGame}>Почати знову</button>}
+      {gameOver && <Button onClick={startNewGame} variant="contained" className='button'>Почати знову</Button>}
     </div>
   );
 }
